1331e29078c68f7996bd6029cbdf9cb1ce8ede80
cedricwillems1 <32699458+cedricwillems1@users.noreply.github.com>
2018-07-18 11:47:07 +0200

    Create Plot6.R

---
+++ Plot6.R
@@ -0,0 +1,18 @@
+> NEI <- readRDS("\\\\fmecwm.local/user/home/15473/Documents/Data Science Coursera CW/Peergraded_course4/summarySCC_PM25.rds")
+> SCC <- readRDS("\\\\fmecwm.local/user/home/15473/Documents/Data Science Coursera CW/Peergraded_course4/Source_Classification_Code.rds")
+
+> library(ggplot2)
+
+> graph1 <- NEI[NEI$fips == "24510" & NEI$type == "ON-ROAD",]
+> graph1 <- aggregate(Emissions ~ year, graph1, sum)
+> graph1 <- cbind(Counties = rep("Baltimore City", 4),graph1)
+> graph2 <- NEI[NEI$fips == "06037" & NEI$type == "ON-ROAD",]
+> graph2 <- aggregate(Emissions ~ year, graph2, sum) 
+> graph2 <- cbind(Counties = rep("Los Angeles", 4),graph2)
+> graph <- rbind(graph1, graph2)
+> rm(graph1, graph2)
+> png(filename = "\\\\fmecwm.local/user/home/15473/Documents/Data Science Coursera CW/Peergraded_course4/plot6.png", width = 480, height = 480, units = "px", bg = "white")
+> g <- ggplot(graph, aes(year, Emissions, col = Counties))
+> g <- g + geom_line() + xlab("Year") + ylab("Total Emissions from Motor Sources") + ggtitle("Comparison of Motor Source Related Emissions between two Counties over the years")
+> print(g)
+> dev.off()
